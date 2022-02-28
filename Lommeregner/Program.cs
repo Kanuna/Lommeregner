79f3cfccc2b1f623a5fdaa5f274aa799c3c51fec
KanunaHaskai <mathiasbojelarsen03@gmail.com>
2022-02-28 10:43:33 +0100

    Changedwriteline to write

--- Lommeregner/Program.cs
+++ Lommeregner/Program.cs
@@ -3,13 +3,13 @@ int num2;
 int result = 0;
 char symbol;
 
-Console.WriteLine("Tal 1: ");
+Console.Write("Tal 1: ");
 num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Tal 2: ");
+Console.Write("Tal 2: ");
 num2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Symbol +, -, /, *: ");
+Console.Write("Symbol +, -, /, *: ");
 symbol = Convert.ToChar(Console.ReadLine());
 
 Console.WriteLine("result: " + result);
