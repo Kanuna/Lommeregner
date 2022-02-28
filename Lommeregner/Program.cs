faa3b83c2d9e393b83ff51a184cf6101086f0ea2
KanunaHaskai <mathiasbojelarsen03@gmail.com>
2022-02-28 10:38:47 +0100

    Looks better

--- Lommeregner/Program.cs
+++ Lommeregner/Program.cs
@@ -9,21 +9,15 @@ num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Tal 2: ");
 num2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Symbol + - / *: ");
+Console.WriteLine("Symbol +, -, /, *: ");
 symbol = Convert.ToChar(Console.ReadLine());
 
 Console.WriteLine("result: " + result);
 
 
-int Multiply(int num1, int num2)
-{
-    return result = num1 * num2;
-}
+int Multiply(int num1, int num2) => result = num1 * num2;
 
-int Divide(int num1, int num2)
-{
-    return result = num1 / num2;
-}
+int Divide(int num1, int num2) => result = num1 / num2;
 
 int Plus(int num1, int num2) => result = num1 + num2;
 
