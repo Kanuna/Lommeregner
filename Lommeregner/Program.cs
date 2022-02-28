3c0d7bc62a2e6dd778bbe55efe0bcdfe6156a1c9
rumbo <rumbo.9370@gmail.com>
2022-02-28 10:27:17 +0100

    main

--- Lommeregner/Program.cs
+++ Lommeregner/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num1;
+int num2;
+int result;
+
+
+
+Console.WriteLine("intast to tal");
+Console.WriteLine("tal1");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("tal2");
+num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("tryk på en tast");
+Console.ReadLine();
