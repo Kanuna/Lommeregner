4cefe07234317b3162199f004ec9447807f8e63b
KanunaHaskai <mathiasbojelarsen03@gmail.com>
2022-02-28 10:33:31 +0100

    Multiply & divide

--- Lommeregner/Program.cs
+++ Lommeregner/Program.cs
@@ -1,5 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-int num1;
+﻿int num1;
 int num2;
+int result = 0;
+char symbol;
+
+Console.WriteLine("Tal 1: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Tal 2: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Symbol + - / *: ");
+symbol = Convert.ToChar(Console.ReadLine());
+
+Console.WriteLine("result: " + result);
+
+
+int Multiply(int num1, int num2)
+{
+    return result = num1 * num2;
+}
+
+int Divide(int num1, int num2)
+{
+    return result = num1 / num2;
+}
